b88b0baf4ab1d86fc1da2988f2f5a34da398baca
cannelle28 <wioletta.manka728@gmail.com>
2017-02-15 17:06:10 +0100

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a sentence you want to reverse:");
+
+            string sentence = Console.ReadLine();
+            Console.WriteLine(ReverseString(sentence));
+            Console.Read();
+
+        }
+            public static string ReverseString(string s)
+            {
+                char[] arr = s.ToCharArray();
+                Array.Reverse(arr);
+                return new string(arr);
+            }
+    }
+}
